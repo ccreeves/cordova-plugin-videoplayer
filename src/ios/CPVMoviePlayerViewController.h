0d701527e14b2e86c01a7366827cabcdf8d90344
kent013 <kentaro.ishitoya@gmail.com>
2015-03-30 21:24:11 +0900

    Added ios implementation stab

---
+++ src/ios/CPVMoviePlayerViewController.h
@@ -0,0 +1,13 @@
+//
+//  CPVMoviePlayerViewController.h
+//  cordova-plugin-videoplayer
+//
+//  Created by ISHITOYA Kentaro on 2015/03/30.
+//
+//
+
+@import MediaPlayer;
+
+@interface CPVMoviePlayerViewController : MPMoviePlayerViewController
+@end
+/Users/ishitoya/Dropbox/repository/cordova-plugin-videoplayer/plugin.xml
---
+++ src/ios/CPVMoviePlayerViewController.m
@@ -0,0 +1,27 @@
+//
+//  CPVMoviePlayerViewController.m
+//  cordova-plugin-videoplayer
+//
+//  Created by ISHITOYA Kentaro on 2015/03/30.
+//
+//
+
+#import "CPVMoviePlayerViewController.h"
+
+@interface CPVMoviePlayerViewController ()
+
+@end
+
+@implementation CPVMoviePlayerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
