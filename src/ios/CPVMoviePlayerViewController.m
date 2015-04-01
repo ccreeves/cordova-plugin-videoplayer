5e728aa9fdeb67a962714b287eedb58d5b513739
kent013 <kentaro.ishitoya@gmail.com>
2015-04-01 17:18:10 +0900

    Added NSLog

--- src/ios/CPVMoviePlayerViewController.m
+++ src/ios/CPVMoviePlayerViewController.m
@@ -16,15 +16,4 @@
 - (void)showMessage:(CDVInvokedUrlCommand*)command{
 	NSLog(@"%@", command);
 }
-
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
 @end
